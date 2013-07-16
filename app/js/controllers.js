070bc06450a4b16f8b2a3b7ff8556a8b97a84c82
syedshahul <syedshahul2002@gmail.com>
2013-07-16 15:22:53 +0530

    adding bootstrap ui

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,38 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('bibliotek.controllers', [])
+    .controller('MyCtrl1', ['$scope', function ($scope) {
+    $scope.text = "view one";
+}])
+    .controller('MyCtrl2', ['$scope', function ($scope) {
+    $scope.message = "view two";
+}])
+    /*
+     * We could config like below:
+     *  .controller('AccordionDemoCtrl', ['$scope', function (skop) {
+     * skop.oneAtATime = true;
+     * ...
+     * */
+    .controller('AccordionDemoCtrl', ['$scope', function ($scope) {
+    $scope.oneAtATime = true;
 
-  }])
-  .controller('MyCtrl2', [function() {
+    $scope.groups = [
+        {
+            title:"Dynamic Group Header - 1",
+            content:"Dynamic Group Body - 1"
+        },
+        {
+            title:"Dynamic Group Header - 2",
+            content:"Dynamic Group Body - 2"
+        }
+    ];
 
-  }]);
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function () {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
+}]);
+
